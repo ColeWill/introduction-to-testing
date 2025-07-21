fb2ab51984d397fa3c7aefc6b9096ca4752c688a
colewill <wcole2222@gmail.com>
2025-07-21 16:10:47 -0600

    finished section 1, added some initial tests and now moving on to section 2 on referential equality

--- examples/basic-math/src/arithmetic.test.js
+++ examples/basic-math/src/arithmetic.test.js
@@ -1,9 +1,61 @@
 import { describe, it, expect } from 'vitest';
+import { add, multiply, subtract, divide } from './arithmetic.js';
 
-describe.todo('add', () => {});
+describe('add', () => {
+    it('should add 2 positive numbers', () =>{
+        expect(add(2,2)).toBe(4)
+    });
 
-describe.todo('subtract', () => {});
+    it('should add 2 negative numbers', () => {
+        expect(add(-1, -4)).toBe(-5);
+    });
 
-describe.todo('multiply', () => {});
+    it('should parse strings into numbers', () => {
+        expect(add('1', '190')).toBe(191);
 
-describe.todo('divide', () => {});
+        expect(add('-2', '-7')).toBe(-9);
+    });
+    it('should get angry if you dont give the function a number that can be parsed', () => {
+        expect(() => add('potato', 2)).toThrow();
+    });
+});
+
+describe('subtract', () => {
+    it('should subtract 2 positive numbers', ()=>{
+        expect(subtract(10, 5)).toBe(5);
+    });
+
+    it('should subtract all of the numbers if you pass an array as the first argument',  () => {
+        expect(subtract([10,5], 2)).toBe(3);
+    });
+
+    it('should default a value to zero if no arg is passed', ()=>{
+        expect(subtract()).toBe(0);
+        expect(subtract(3)).toBe(3);
+    })
+
+    it.skip('should throw an error if there is an undefined passed in', () => {
+        expect(() => subtract(10, undefined)).toThrow();
+    });
+});
+
+describe('multiply', () => {
+    it('should multiply 2 positive numbers', ()=>{
+        expect(multiply(2,4)).toBe(8);
+    });
+
+    it('should throw an err if one of the args is null', () => {
+        expect(()=> multiply(undefined, null)).toThrow();
+    });
+});
+
+describe('divide', () => {
+    it('should divide 2 numbers', ()=> {
+        expect(divide(8,4)).toBe(2);
+    });
+    
+    it('should return null if you divide by zero', () => {
+        expect(divide(0, 19)).toBeNull();
+        expect(divide(19, 0)).toBeNull();
+    });
+});
