e8360da6f6a192f8f900711693bf86b35e956d8d
colewill <wcole2222@gmail.com>
2025-07-23 10:47:14 -0600

    added some tests to the jsx 'accident-counter' example

--- examples/element-factory/src/button.test.js
+++ examples/element-factory/src/button.test.js
@@ -1,9 +1,37 @@
+import { fireEvent, screen } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
 import { createButton } from './button.js';
 
-describe.todo('createButton', () => {
-  it('should create a button element', () => {});
+describe('createButton', () => {
 
-  it('should have the text "Click Me"', () => {});
+  afterEach(()=> {
+    document.body.innerHTML = "";
+  })
 
-  it('should change the text to "Clicked!" when clicked', async () => {});
+  it('should create a button element', () => {
+    // const button = createButton();
+    document.body.appendChild(createButton());
+
+    const button = screen.getByRole('button', {name: 'Click Me'});
+
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should have the text "Click Me"', () => {
+    const button = createButton();
+
+    expect(button.textContent.toLowerCase()).toBe('click me');
+  });
+
+  it('should change the text to "Clicked!" when clicked', async () => {
+    document.body.appendChild(createButton());
+
+    const button = screen.getByRole('button', {name: 'Click Me'});
+
+    // fireEvent(button, new MouseEvent('click'))
+
+    await userEvent.click(button);
+
+    expect(button.textContent.toLowerCase()).toBe('clicked!')
+  });
 });
