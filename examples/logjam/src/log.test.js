f30c482a955b5bb5137053546917a31260c03744
colewill <wcole2222@gmail.com>
2025-07-24 16:59:58 -0600

    added somed tests to the logger

--- examples/logjam/src/log.test.js
+++ examples/logjam/src/log.test.js
@@ -1,4 +1,43 @@
 import { expect, it, vi, beforeEach, afterEach, describe } from 'vitest';
 import { log } from './log';
+import { sendToServer } from './send-to-server';
 
-describe.todo('logger', () => {});
+vi.mock('./send-to-server', () => ({
+  sendToServer: vi.fn(),
+})); // overrides the entire file
+
+describe('logger', () => {
+  describe('development', () => {
+    beforeEach(() => {
+      vi.stubEnv('MODE', 'development');
+    });
+    afterEach(() => {
+      vi.restoreAllMocks();
+    });
+
+    it('logs to the console in dev mode', () => {
+      const logSpy = vi.spyOn(console, 'log');
+
+      log('hey');
+
+      expect(logSpy).toHaveBeenCalledTimes(1);
+      expect(logSpy).toHaveBeenCalledWith('hey');
+    });
+  });
+
+  describe('production', () => {
+    beforeEach(() => {
+      vi.stubEnv('MODE', 'production');
+    });
+    afterEach(() => {
+      vi.restoreAllMocks();
+    });
+    it('does not call console.log in production', () => {
+      const logSpy = vi.spyOn(console, 'log');
+      log('hey again');
+
+      expect(logSpy).not.toHaveBeenCalled();
+      expect(sendToServer).toHaveBeenCalled();
+    });
+  });
+});
--- examples/logjam/src/send-to-server.js
+++ examples/logjam/src/send-to-server.js
@@ -4,5 +4,6 @@
  * @param {string} message
  */
 export const sendToServer = (level, message) => {
+  throw new Error('This code should not run at all!');
   return `You must mock this function: sendToServer(${level}, ${message})`;
 };
